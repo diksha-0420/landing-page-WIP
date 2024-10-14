651b0f92e21f5b902dc1bf8b3884d261ece3940e
diksha-0420 <diksharahi2006@gmail.com>
2024-10-14 20:32:18 +0530

    your first comment

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
         }}
       >
         <h1 className="text-4xl sm:text-5xl font-bold text-white">
-          We are coder group
+          We are coder vghvhgh
         </h1>
 
         <button
